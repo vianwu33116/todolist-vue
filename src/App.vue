1fcef3280066b2f79a2b46277bb3d52e47dc2db1
vianwu33116 <wserena33116@gmail.com>
2024-08-25 16:43:11 +0800

    remove router link

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,8 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">login</RouterLink> | <RouterLink to="/signup">signup</RouterLink> |
-        <RouterLink to="/todo">todo</RouterLink>
-      </nav>
-    </div>
-  </header>
   <RouterView />
 </template>
 
