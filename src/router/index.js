11a1b0c3e836e86c607c466dcf7cf9e1ce376ef2
vianwu33116 <wserena33116@gmail.com>
2024-08-25 16:42:33 +0800

    add 404 notFound page

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/TodoView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: () => import('../views/NotFoundView.vue')
     }
   ]
 })
---
+++ src/views/NotfoundView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container d-flex flex-column justify-content-center">
+    <h2 class="text-center highlight-color mt-5 mb-4">404</h2>
+    <h2 class="text-center highlight-color">Page Not Found</h2>
+  </div>
+</template>
